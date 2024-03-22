203830499ba1c56c7d168fb7e291552282d6bb19
ColinVIgnatious <colinv.ignatious@gmail.com>
2024-03-22 23:04:15 +0530

    fifth commit

--- src/contexts/sessionprovider.jsx
+++ src/contexts/sessionprovider.jsx
@@ -1,19 +1,33 @@
 "use client";
-import React, { createContext, useState,useEffect } from 'react'
-const SessionContext=createContext();
-function Sessionprovider({children}) {
-    const[token,setToken]=useState(localStorage.getItem("token")||"");
-    useEffect(() => {
-      localStorage.setItem("token",token)
-    }, [token])
-    
+import React, { createContext, useState, useEffect } from 'react';
+
+const SessionContext = createContext();
+
+function SessionProvider({ children }) {
+  const [token, setToken] = useState(() => {
+    // Check if localStorage is available
+    if (typeof window !== 'undefined') {
+      return localStorage.getItem("token") || "";
+    } else {
+      return "";
+    }
+  });
+
+  useEffect(() => {
+    // Check if localStorage is available
+    if (typeof window !== 'undefined') {
+      localStorage.setItem("token", token);
+    }
+  }, [token]);
+
   return (
-    <SessionContext.Provider value={{token,setToken}}>
-        {children}
+    <SessionContext.Provider value={{ token, setToken }}>
+      {children}
     </SessionContext.Provider>
-        
-  )
+  );
 }
-export {SessionContext}
-export default Sessionprovider
+
+export { SessionContext };
+export default SessionProvider;
+
 
