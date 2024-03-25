f88310c9bf8f2fcf048987da17e82b4dcb489a28
ColinVIgnatious <colinv.ignatious@gmail.com>
2024-03-25 11:12:50 +0530

    sixth commit

--- src/app/blogdetail/[id]/page.jsx
+++ src/app/blogdetail/[id]/page.jsx
@@ -3,7 +3,7 @@ import React, { useState, useContext, useEffect } from "react";
 import Image from "next/image";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import { DM_Serif_Display } from "next/font/google";
+import { DM_Serif_Display,Source_Code_Pro } from "next/font/google";
 import axios from "axios";
 import { useParams } from "next/navigation";
 import { SessionContext } from "@/contexts/sessionprovider";
@@ -12,6 +12,9 @@ import ImageBlog from '@/components/imageBlog'
 const dmSerifDisplay = DM_Serif_Display({
     subsets: ["latin"],
     weight: ["400"],
+  });
+const sourceCodePro = Source_Code_Pro({
+    subsets: ["latin"],
   });
 
 function Blogdetail() {
@@ -47,7 +50,7 @@ function Blogdetail() {
         <div className="bg-[#FDF2DB]">
           <div className={`text-[#FDF2DB] bg-[#842A3A] text-xl indent-5 align-middle min-h-16 rounded-b-xl ${dmSerifDisplay.className}`}>{post.title.rendered}</div>
           {post && <div className="w-[1312] h-[800]"><ImageBlog id={post.id}/></div>}
-          <div dangerouslySetInnerHTML={{ __html:post.content.rendered }} />
+          <div className={`text-md ${sourceCodePro.className}`} dangerouslySetInnerHTML={{ __html:post.content.rendered }} />
         </div>
       <Footer/>
     </>
